0f0e8841b4deedbeff3f7145abcaf611b255a7d5
Jacques Trepanier <jtrepanier2000@gmail.com>
2026-02-16 19:35:24 -0800

    Added markdown rendering and keymaps for telescope find file

--- init.lua
+++ init.lua
@@ -19,3 +19,6 @@ vim.lsp.enable("quick_lint_js")
 
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts) -- Go to declaration
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts) -- Go to definition
+
+local ts_builtin = require('telescope.builtin')
+vim.keymap.set('n', 'ff', ts_builtin.find_files, { desc = "Find files" })
